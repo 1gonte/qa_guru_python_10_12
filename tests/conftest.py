771bf1c65a4918d8d54ff07e8b095bb147b3bba4
Georgii Sergeev <g.sergeev@big3.ru>
2024-02-26 19:18:02 +0300

    Hide secret data to .env file & Add allure tags

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,17 @@
+import os
+
 import pytest
-from selene import browser, Config
+from dotenv import load_dotenv
+from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from qa_guru_python_10_12.utils import attach
+
+
+@pytest.fixture(scope='function', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -25,8 +33,12 @@ def browser_management():
     }
 
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options)
 
     browser.config.driver = driver
@@ -37,4 +49,5 @@ def browser_management():
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_video(browser)
+
     browser.quit()
--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,9 +1,16 @@
 import allure
+from allure_commons.types import Severity
 
 from qa_guru_python_10_12.data.users import student
 from qa_guru_python_10_12.pages.registartion_page import RegistrationPage
 
 
+@allure.tag('DemoQa')
+@allure.severity(Severity.CRITICAL)
+@allure.label('Owner', '1gonte')
+@allure.feature('User registration')
+@allure.story('Register new user')
+@allure.link('https://demoqa.com', name='Testing')
 @allure.title('Successfull registration')
 def test_fill_registration_form():
 
